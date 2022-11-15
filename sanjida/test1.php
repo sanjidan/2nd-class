086a7ce9df20c17076367cec10ed827de7fb4252
meherun <115523585+sanjidan@users.noreply.github.com>
2022-11-15 11:49:45 +0600

    Add files via upload

---
+++ sanjida/test1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+<div class="sanjida">
+
+<h1> Institute: Dhaka Mohila Polytechnic Institute</h1>
+<h2>Name:Meherun Nur Sanjida</h2>
+<h3>Department: Computer </h3>
+<h4>Semister:8th</h4>
+<h5>Shift:2nd</h5>
+<h6>Roll:143559</h6>
+  <?php
+//   echo' <h1>hello world!</h1>';
+?>
+</div>
+<div class="new">
+    <h5>menu</h5>
+</div>
+</body>
+</html>
